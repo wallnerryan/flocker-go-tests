b43c088fecce8a49f10a9bee962c0aa3fe00d363
Ryan Wallner <ryan.wallner@clusterhq.com>
2016-03-11 19:34:11 +0000

    simple test

---
+++ flocker.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	flockerclient "github.com/ClusterHQ/flocker-go"
+)
+
+func main() {
+	client, err := flockerclient.NewClient("ip-172-20-0-183", 4523, "172.20.0.182", "/etc/flocker/cluster.crt", "/etc/flocker/api.key", "/etc/flocker/api.crt")
+	if err != nil {
+		fmt.Printf("Could not create client")
+	}
+
+	id, err := client.GetDatasetID("testvol-poc")
+	if err != nil {
+        	fmt.Println(err)
+        	fmt.Printf("Could not find dataset ID")
+	}
+	fmt.Printf(id+"\n")
+}
+
